b207735ea00120a1dc34675bd1b07b41e6e9b6d4
davidelunetta <davidelunetta.27@gmail.com>
2023-11-13 15:42:30 +0100

    added header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+       <h1 class="text-center py-3">{{ store.title }}</h1> 
+    </header>
+</template>
+
+<script>
+import { store } from "../data/store";
+export default{
+    name:'HeaderComponent',
+
+    data(){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
