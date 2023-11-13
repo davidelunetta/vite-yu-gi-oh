0e4395dd202c96ccec7da4ddbe086b28aa85d837
davidelunetta <davidelunetta.27@gmail.com>
2023-11-13 15:42:48 +0100

    added header component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <figure><img :src="img" alt="img"></figure>
+        <h5>Name: {{ name }}</h5>
+        <div>Type: {{ type }}</div>
+        <div>Frame Type: {{ frameType }}</div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'CardComponent',
+    props:{
+        img:String,
+        name:String,
+        type:String,
+        frameType:String,
+    },
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
